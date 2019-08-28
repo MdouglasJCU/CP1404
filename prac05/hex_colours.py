a8f0dad5672d5369fa1a8330a47bc947e0c2759e
mitchell <mitchell.douglas@my.jcu.edu.au>
2019-08-29 08:31:44 +1000

    Simplified while statement

--- prac05/hex_colours.py
+++ prac05/hex_colours.py
@@ -1,14 +1,11 @@
 colours = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "azure1": "#f0ffff", "beirge": "#f5f5dc", "black": "#000000", "brown": "#a52a2a", "burlywood": "deb887", "CadetBlue": "#5f9ea0", "coral": "#ff7f50", "DarkGreen": "#006400"}
 
 colour = input("Please choose a colour: ")
-false_input = False
 
-while not false_input:
+while colour != "":
     if colour in colours:
         print("The hexadecimal colour of {} is {}".format(colour, colours[colour]))
         get_colour = input("Please choose a colour: ")
-    elif colour == "":
-        print("Thanks for choosing colours")
-        false_input = True
     else:
+        print("Incorrect colour")
         colour = input("Please choose a colour: ")
