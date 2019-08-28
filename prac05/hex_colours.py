0b591b140116708fb592740d0c9a4d843ab36baa
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-08-28 12:42:55 +1000

    changed get_colour to colour

--- prac05/hex_colours.py
+++ prac05/hex_colours.py
@@ -1,14 +1,14 @@
 colours = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "azure1": "#f0ffff", "beirge": "#f5f5dc", "black": "#000000", "brown": "#a52a2a", "burlywood": "deb887", "CadetBlue": "#5f9ea0", "coral": "#ff7f50", "DarkGreen": "#006400"}
 
-get_colour = input("Please choose a colour: ")
+colour = input("Please choose a colour: ")
 false_input = False
 
 while not false_input:
-    if get_colour in colours:
-        print("The hexadecimal colour of {} is {}".format(get_colour, colours[get_colour]))
+    if colour in colours:
+        print("The hexadecimal colour of {} is {}".format(colour, colours[colour]))
         get_colour = input("Please choose a colour: ")
-    elif get_colour == "":
+    elif colour == "":
         print("Thanks for choosing colours")
         false_input = True
     else:
-        get_colour = input("Please choose a colour: ")
+        colour = input("Please choose a colour: ")
