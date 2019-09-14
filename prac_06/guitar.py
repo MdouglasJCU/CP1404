477af151c5f32f71a4c620c73a3a32604c8047a1
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-09-14 18:41:47 +1000

    added main functions in guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+class guitar:
+
+    def __init__(self):
+        self.name = ""
+        self.year = 0
+        self.cost = 0
+
+    def __str__(self):
+        
+
+    def get_age(self):
+
+
+    def is_vintage(self):
