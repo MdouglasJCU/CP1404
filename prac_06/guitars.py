fbb6ef0bfdd22dc143b19854658b4b204964d792
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-09-14 21:48:06 +1000

    changed cost to float Added if/else statement for printing and identifying if vintage print the guitars is a nicely formatted list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,19 +1,31 @@
 from guitar import guitar
 
+
 def main():
     guitars = []
 
-    print("My guitars!")
+    print("My guitars")
 
     name = input("Enter your guitars name: ")
 
     while name != "":
         year = int(input("Year: "))
-        cost = int(input("Cost: "))
+        cost = float(input("Cost: $"))
         add_guitar = guitar(name, year, cost)
         guitars.append(add_guitar)
         print(add_guitar, "added.")
         name = input("Enter your guitars name: ")
+
+    if guitars:
+        guitars.sort()
+        print("These are my guitars:")
+        for i, Guitar in enumerate(guitars):
+            vintage_string = ""
+            if Guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {0}: {1.name} ({1.year}), worth ${1.cost:5,.2f} {2}".format(i + 1, Guitar, vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
 
 
 main()
