ce190601428cc087d7ccc1e7cd3fc417f649b6bb
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-09-14 14:05:32 +1000

    Testing programming_language.py as per exercise

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,8 +2,11 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-
+    print(ruby, python, visual_basic)
 
 main()
 
