582ec1e0b497575db0d3da67f7ec76d74aa53d82
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-09-14 18:47:48 +1000

    corrected __init_ method, added to __str__ and get_age method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,14 +1,14 @@
 class guitar:
 
-    def __init__(self):
-        self.name = ""
-        self.year = 0
-        self.cost = 0
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
 
     def __str__(self):
-        
+        return "{} ({}) : {}".format(self.name, self.year, self.cost)
 
     def get_age(self):
-
+        return 2019 - self.year
 
     def is_vintage(self):
