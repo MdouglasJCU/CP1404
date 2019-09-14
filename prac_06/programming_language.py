279359ec408bb6f0dcf5459c4decfc42f53dea08
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-09-14 14:05:10 +1000

    added is_dynamic and __str__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,7 +1,15 @@
 class ProgrammingLanguage:
 
-    def __init__(self):
+    def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dyanmic(self):
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        return "{}, {} typing, reflection={}, First appeared in {}".format(self.name, self.year, self.reflection,
+                                                                           self.year)
+
