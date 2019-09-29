3095fb6bc433afbf7e745e7fc7833d92e426c82e
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-09-29 17:26:55 +1000

    added Test Taxi class

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,15 @@
+from taxi import Taxi
+
+
+def main():
+    """Test Taxi class"""
+    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi.start_fare()
+    taxi.drive(40)
+    print("{}.  Current total fare is ${}".format(taxi, taxi.get_fare()))
+    taxi.start_fare()
+    taxi.drive(100)
+    print("{}.  Current total fare is ${}".format(taxi, taxi.get_fare()))
+
+
+main()
