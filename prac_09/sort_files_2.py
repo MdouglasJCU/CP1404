f37e444f40c5dab73a9cbcff6ddacf15120a0478
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-10-13 13:58:43 +1000

    Create folder based on user input. Move file with specific extension into created folder

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -5,13 +5,24 @@ def main():
     """Move files into folders"""
     os.chdir("FilesToSort")
 
+    ext_to_category = {}
+
     for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
 
         ext = filename.split('.')[-1]
         # used to testing
-        print(ext)
+        # print(ext)
 
+        if ext not in ext_to_category:
+            category = input("What category would you like to sort {} files into? ".format(ext))
+            ext_to_category[ext] = category
+            try:
+                os.mkdir(category)
+            except EnvironmentError:
+                pass
+
+        os.rename(filename, "{}/{}".format(ext_to_category[ext], filename))
 
 main()
