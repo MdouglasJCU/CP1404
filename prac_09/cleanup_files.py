dd9710dab2696df3f1310d885f32dfb744d84643
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-10-13 12:40:28 +1000

    initial commit

---
+++ prac_09/cleanup_files.py
@@ -0,0 +1,11 @@
+import os
+
+def main():
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+main()
