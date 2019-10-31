a76c4f67ff212be43eeb3b237f0e73dae95b07bb
MitchellDouglas <mitchell.douglas@my.jcu.edu.au>
2019-10-31 21:33:03 +1000

    Incorporated Error Handling Print Wiki page title, summary and URL

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,11 +2,20 @@ import wikipedia
 
 
 def main():
-    wikipedia_page = input("Enter a page title or seacrh phrase")
+    wikipedia_page = input("Enter a page title or search phrase")
 
     while wikipedia_page != "":
-        print(wikipedia.summary(wikipedia_page))
-        wikipedia_page = input("Enter a page title or seacrh phrase")
+        try:
+            page_title = wikipedia.page(wikipedia_page)
+            print(page_title.title)
+            print(wikipedia.summary(wikipedia_page))
+            print(page_title.url)
+            wikipedia_page = input("Enter a page title or search phrase")
+        # Monty didn't work for me, so used Mercury as the example.
+        # Looks like I've done it right though..
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(e.options)
+            wikipedia_page = input("Enter a page title or search phrase")
 
 
 main()
